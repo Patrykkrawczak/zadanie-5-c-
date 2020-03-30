d30a4db3cb0dbc8a3961021c357f3830ddac6619
Patryk Krawczak <patryk.krawczak@lingarogroup.com>
2020-03-30 16:29:30 +0200

    Add project files.

---
+++ WindowsFormsApp5/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsFormsApp;
+
+namespace WindowsFormsApp5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            float true_wynik = 1000000 / 3;
+
+            double n = int.Parse(k_box.Text);
+            n = Math.Pow(10, n);
+            List<int> wartosci = new List<int>();
+
+            
+            Random rnd = new Random();
+            int d = 10;
+            int l = 100000;
+            double f = 0.0F;
+            int o = 0;
+            WindowsFormsApp.SingleCount single = new SingleCount();
+            single.X1 = 0;
+            single.X2 = 100;
+            double f2 = 0.0F;
+            for (int j = -10; j < 10; j++)
+            {
+                for (int j2 =-10; j2 < 10;j2++) {
+                    single.X1 = j;
+                    single.X2 = j2;
+                    f = 0.0F;
+                    f2 = 0.0F;
+                    int licz = 0;
+                    double old_i = 0;
+
+                    for (double i = single.X1; i <= single.X2; i = i + 100F / n)
+                    {
+
+                        licz += 1;
+                        f += (i * i) * (i - old_i);
+
+                        old_i = i;
+                    };
+                    for (double i = single.X1; i <= single.X2; i = i + 100F / n)
+                    {
+
+                        licz += 1;
+                        f2 += (i * i * i) * (i - old_i);
+
+                        old_i = i;
+                    };
+
+                    if (f == f2) 
+                    {
+                        string wynik = " prostokaty   x1 = " + single.X1.ToString()+ " x2 = "+ single.X2.ToString();
+                        wynik_box.Text += wynik;
+                    }
+
+                    f = 0.0F;
+                    f2 = 0.0F;
+                    old_i = 0;
+                    for (double i = 0.0F; i <= 100.0; i = i + 100F / n)
+                    {
+
+                        licz += 1;
+                        f += (((i * i) + (old_i * old_i)) / 2) * (i - old_i);
+
+                        old_i = i;
+                    };
+                    for (double i = 0.0F; i <= 100.0; i = i + 100F / n)
+                    {
+
+                        licz += 1;
+                        f2 += (((i * i* i) + (old_i * old_i * old_i)) / 2) * (i - old_i);
+
+                        old_i = i;
+                    };
+                    if (Math.Floor(f) == Math.Floor(f2))
+                    {
+                        string wynik = " trapezy   x1 = " + single.X1.ToString() + " x2 = " + single.X2.ToString();
+                        wynik_box.Text += wynik;
+                    }
+                };
+            };
+        }
+
+    }
+}
---
+++ WindowsFormsApp5/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp
+{
+    public enum AreaType
+    {
+        Rectangle,
+        Trapezoid
+    }
+    public class SingleCount
+    {
+        public double X1 { get; set; }
+        public double X2 { get; set; }
+        public int N { get; set; }
+        public AreaType AreaType { get; set; }
+        public double Area { get; set; }
+        public int CalculationNumber { get; set; }
+        public int LowestN { get; set; }
+        public double MinSquareError { get; set; }
+
+        public SingleCount() { }
+        public SingleCount(double x1, double x2, int n, AreaType areaType, double area, int calcNumber) { this.X1 = x1; this.X2 = x2; this.N = n; this.AreaType = areaType; this.Area = area; this.CalculationNumber = calcNumber; }
+        public SingleCount(double x1, double x2, int n, AreaType areaType, double area, int calcNumber, int lowestN) { this.X1 = x1; this.X2 = x2; this.N = n; this.AreaType = areaType; this.Area = area; this.CalculationNumber = calcNumber; this.LowestN = lowestN; }
+    }
+}
+
+namespace WindowsFormsApp5
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
